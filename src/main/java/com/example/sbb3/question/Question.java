f5e007b55427e4039da8871d67ec2257d29c0583
6uiwj <oterges00@gmail.com>
2024-09-05 22:38:38 +0900

    240905 - Define Entity

---
+++ src/main/java/com/example/sbb3/question/Question.java
@@ -0,0 +1,30 @@
+package com.example.sbb3.question;
+
+import com.example.sbb3.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity //이 클래스를 엔티티로 지정
+public class Question {
+    @Id //기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    //부모 쪽에서 관계 설정(mappedBy = 부모엔티티를 참조하는 자식 엔티티의 속성)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
