06d053ae73ab7fd2de4f8054b77f2df5ddbf7cef
6uiwj <oterges00@gmail.com>
2024-09-08 21:56:10 +0900

    240908 - 데이터 조회

--- src/test/java/com/example/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/example/sbb3/Sbb3ApplicationTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Sbb3ApplicationTests {
@@ -16,18 +18,23 @@ class Sbb3ApplicationTests {
 
 	@Test
 	void testJPA() {
-		Question q1 = new Question();
-		q1.setContent("테스트 내용1");
-		q1.setSubject("테스트 제목1");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);
+//		Question q1 = new Question();
+//		q1.setContent("테스트 내용1");
+//		q1.setSubject("테스트 제목1");
+//		q1.setCreateDate(LocalDateTime.now());
+//		questionRepository.save(q1);
+//
+//		Question q2 = new Question();
+//		q2.setSubject("테스트 제목2");
+//		q2.setContent("테스트 내용2");
+//		q2.setCreateDate(LocalDateTime.now());
+//		questionRepository.save(q2);
 
-		Question q2 = new Question();
-		q2.setSubject("테스트 제목2");
-		q2.setContent("테스트 내용2");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
 
+		Question q = all.get(0);
+		assertEquals("테스트 제목1", q.getSubject());
 	}
 
 }
