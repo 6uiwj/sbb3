31cf0872c0655e88940a05df90dfc2b53264abae
6uiwj <oterges00@gmail.com>
2024-09-22 23:42:34 +0900

    240922 - createAnswer

--- src/main/java/com/example/sbb3/answer/AnswerController.java
+++ src/main/java/com/example/sbb3/answer/AnswerController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
+
     @PostMapping("/create/{id}")
-    public String createAnswer(@PathVariable("id") Integer id, Model model) {
+    public String createAnswer(@PathVariable("id") Integer id, Model model, @RequestParam(value="content") String content) {
         Question question = this.questionService.getQuestion(id);
-        //저장
+        this.answerService.create(question, content );
         return String.format("redirect:/question/detail/%s",id);
     }
+
 }
