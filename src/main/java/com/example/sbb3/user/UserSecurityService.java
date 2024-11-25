005c3b9a02ef7ae32bc42edeccd3e3f9c2b4e505
6uiwj <oterges00@gmail.com>
2024-11-25 20:37:17 +0900

    241125 - Logout, author

--- src/main/java/com/example/sbb3/user/UserSecurityService.java
+++ src/main/java/com/example/sbb3/user/UserSecurityService.java
@@ -20,15 +20,15 @@ public class UserSecurityService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String useranme) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Optional<SiteUser> _siteUser = this.userRepository.findByusername(useranme);
+        Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
         if(_siteUser.isEmpty()) {
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
         }
         SiteUser siteUser = _siteUser.get();
         List<GrantedAuthority> authorities = new ArrayList<>();
-        if("admin".equals(useranme)) {
+        if("admin".equals(username)) {
             authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
         } else {
             authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
